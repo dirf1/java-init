a84d986f9022d6a2ed540d2000a7f635c18d4a9c
imseongkyu <dimsk1991@gmail.com>
2023-06-28 17:00:06 +0900

    test

--- src/p03/Lotto3.java
+++ src/p03/Lotto3.java
@@ -1,12 +1,19 @@
 package p03;
 
+import java.util.Scanner;
+
 public class Lotto3 {
 	public static void main(String[] args) {
-
-		String lottoStr = "1,32,17,42,15";
+		System.out.println("1~45 범위 중 6개 입력");
+		Scanner scanner = new Scanner(System.in);
+		String lottoStr = scanner.nextLine();
 		String[] strs = lottoStr.split(",");
 		for(int i=0; i<strs.length;i++) {
 			System.out.println(strs[i]);
+			
+				
+		
 		}
 	}
 }
+
